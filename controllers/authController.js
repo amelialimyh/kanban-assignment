f8ab1834003a2d37a0245ff8dc1e5e3ae8e80ec9
Crazytrollgirl <crazytrollgirl@stengg.com>
2022-03-09 16:56:24 +0800

    renamed register.pug to new_user.pug and destructured variables from new_user form in authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,9 @@
 
 exports.register = (req, res) => {
+    console.log(req.body);
+
+    // destructure new_user form variables
+    const { name, email, password, passwordConfirm } = req.body;
+
     res.send("Form submitted");
 }
--- index.js
+++ index.js
@@ -10,12 +10,8 @@ require("dotenv").config();
 
 const app = express();
 
-const DB_HOST = process.env.DB_HOST
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_EMAIL = process.env.DB_EMAIL
-const DB_DATABASE = process.env.DB_DATABASE
-const DB_PORT = process.env.DB_PORT
+// destructure DB variables
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_EMAIL, DB_DATABASE, DB_PORT} = process.env;
 
 let db = mysql.createPool({
   connectionLimit: 100,
