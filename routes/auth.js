ab7a0c4f6f8ac1e1310f3d5750e11ec24e47df05
Crazytrollgirl <crazytrollgirl@stengg.com>
2022-03-17 12:42:45 +0800

    required registerController.js in relevant files

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const authController = require('../controllers/authController');
+const registerController = require('../controllers/registerController');
 const resetController = require('../controllers/resetController');
 const emailController = require('../controllers/emailController');
 const router = express.Router();
 
-router.post('/login', authController.login);
+// router.post('/login', authController.login);
 router.post('/register', registerController.register);
 router.post('/resetpassword', resetController.reset);
 router.post('/updateemail', emailController.changeEmail);
