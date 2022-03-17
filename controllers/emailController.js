9af3f0110224838cab7f210f4af1f60b3a302a7a
Crazytrollgirl <crazytrollgirl@stengg.com>
2022-03-17 13:02:00 +0800

    fixed indentation issues in controllers

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -5,13 +5,13 @@ const bcrypt = require('bcryptjs');
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_EMAIL, DB_DATABASE, DB_PORT} = process.env;
 
 const db = mysql.createPool({
-  connectionLimit: 100,
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  email: DB_EMAIL,
-  database: DB_DATABASE,
-  port: DB_PORT
+    connectionLimit: 100,
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    email: DB_EMAIL,
+    database: DB_DATABASE,
+    port: DB_PORT
 });
 
 // change email
--- controllers/registerController.js
+++ controllers/registerController.js
@@ -4,13 +4,13 @@ const bcrypt = require('bcryptjs');
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_EMAIL, DB_DATABASE, DB_PORT} = process.env;
 
 const db = mysql.createPool({
-  connectionLimit: 100,
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  email: DB_EMAIL,
-  database: DB_DATABASE,
-  port: DB_PORT
+    connectionLimit: 100,
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    email: DB_EMAIL,
+    database: DB_DATABASE,
+    port: DB_PORT
 });
 
 exports.register = (req, res) => {
