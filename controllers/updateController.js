95f5d7d2c0547d04266cde833c305ff10514ea7e
Crazytrollgirl <crazytrollgirl@stengg.com>
2022-03-28 15:12:07 +0800

    shows the exisiting role that user belongs to

--- controllers/updateController.js
+++ controllers/updateController.js
@@ -6,6 +6,24 @@ exports.update = (req, res) => {
    // destructure reset password form variables
     const { username, email, role, status, password, passwordConfirm } = req.body;
 
+    
+    // display current user's roles
+    db.query('SELECT usergrp FROM usergroup WHERE username = ?', [username], (error, result, fields) => {
+        if(error) {
+            console.log('currentrole error >>>>>', error);
+        } 
+        //the usergrp values will be at result[0].usergrp;
+        if (result.length > 0) {
+            res.render('update', {
+                currentrole: result[0].usergrp
+            });
+            // exit render
+            return ;
+        }
+    });
+
+    // update user details in both accounts and usergroup table
+    // query database to check if username exists and update user details
     db.query('SELECT * FROM accounts WHERE username = ?', [username], async (error, result) => {
         if(error) {
             console.log(error);
@@ -20,7 +38,8 @@ exports.update = (req, res) => {
                 message: 'Username or Password is invalid!'
             });
             return ;
-        }
+        } 
+
         // validate password to ensure minimum 8 characters that's a mix of alphabets, numbers and special characters but capped at 10
         var validator = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=^.{8,10}$)");
 
@@ -34,7 +53,6 @@ exports.update = (req, res) => {
             res.render('update', {
                 message: "Password has to contain minimum 8 characters but capped at 10 and it has to be a mix of alphabets, numbers and special characters!"
             });
-            // exit the function
             return ;
         }
 
---
+++ models/curentRole.js
@@ -0,0 +1,4 @@
+const res = require('express/lib/response');
+const mysql = require('mysql');
+const db = require('../dbServer');
+
