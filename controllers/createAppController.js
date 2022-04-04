acb45ae19c939ae728d780513db22db0ef458e6d
Crazytrollgirl <crazytrollgirl@stengg.com>
2022-04-04 17:00:10 +0800

    created create application form in createapp.pug

---
+++ controllers/createAppController.js
@@ -0,0 +1,5 @@
+const db = require('../dbServer');
+
+exports.createapp = (req, res) => {
+    
+}
--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,14 @@ const express = require('express');
 const registerController = require('../controllers/registerController');
 const updateController = require('../controllers/updateController');
 const assignRoleController = require('../controllers/assignRoleController');
+const createAppController = require('../controllers/createAppController');
 const createTaskController = require('../controllers/createTaskController');
 const router = express.Router();
 
 router.post('/register', registerController.register);
 router.post('/update', updateController.update);
 router.post('/assignrole', assignRoleController.assign);
+router.post('/createapplication', createAppController.createapp);
 router.post('/createTask', createTaskController.createtask);
 
 
