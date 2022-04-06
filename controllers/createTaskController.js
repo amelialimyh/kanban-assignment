ab39cb5b6882d0d6be24a32a90f0c3f2a0182fb9
Crazytrollgirl <crazytrollgirl@stengg.com>
2022-04-06 17:55:41 +0800

    task is inserted into task table

--- controllers/createTaskController.js
+++ controllers/createTaskController.js
@@ -3,43 +3,51 @@ const db = require('../dbServer');
 exports.createtask = (req, res, fields) => {
     console.log(req.body);
 
-    const { name, description, notes, task_plan, state } = req.body;
+    const { name, description, notes, app_acronym } = req.body;
 
     // check if app exists
-    db.query('SELECT * FROM application WHERE app_acronym = ?', [app_acronym], (error, result) => {
+    db.query('SELECT * FROM application WHERE app_acronym = ?',[app_acronym], (error, result) => {
         if (error) {
             console.log(error);
         }
 
         if (result.length === 0) {
+            console.log('result >>>', result);
             res.render('createtask', {
                 message: 'Application does not exist!'
             });
             return ;
         } else {
-            // new task id
-            var newTaskId = `${result[0].app_acronym}_${result[0].rnumber}`;
-            // current app acronym
-            var task_app_acronym = `${result[0].app_acronym}`;
-            // task creator
-            var task_creator = req.session.username;
-            var task_owner = req.session.username;
-            // date that task was created
-            var today = new Date();
-            var createDate = today.getDate() + '-' + (today.getMonth()+1) + '-' + today.getFullYear();
-            console.log(createDate);
+            for (let i = 0; i < result.length; i++){
+                console.log('for loop result >>>>', result);
+                var task_app_acronym = `${result[i].app_acronym}`;
+                console.log('app_acronnym >>>>', app_acronym);
+                // new task id
+                var newTaskId = `${result[0].app_acronym}_${result[0].rnumber}`;
 
-            db.query('INSERT INTO task (task_id,name,description,notes,task_plan,task_app_acronym,state,creator,owner,createDate) VALUES (?,?,?,?,0,?,?,?,?,?)', [newTaskId, name, description, notes, task_app_acronym, state, task_creator, task_owner, createDate], (error, result) => {
-                if (error) {
-                    console.log('insert application error >>>', error);
-                } else {
-                    res.render('createapp', {
-                        message: 'Application created'
-                    });
-                }
-            });
+                // state
+                var state = 'open';
+    
+                // creator
+                var task_creator = req.session.username;
+                
+                // owner
+                var task_owner = req.session.username;
+    
+                // date that task was created
+                var today = new Date();
+                var createDate = today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate();
+    
+                db.query('INSERT INTO task (task_id,name,description,notes,task_app_acronym,state,creator,owner,createDate) VALUES (?,?,?,?,?,?,?,?,?)', [newTaskId, name, description, notes, task_app_acronym, state, task_creator, task_owner, createDate], (error, result) => {
+                    if (error) {
+                        console.log('insert application error >>>', error);
+                    } else {
+                        res.render('createapp', {
+                            message: 'Application created',
+                        });
+                    }
+                });
+            }
         }
     });
-    
-    
 }
