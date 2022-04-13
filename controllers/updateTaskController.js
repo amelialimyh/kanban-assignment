ed158ccadafdaf18ba9eb764226f64cf3a1ada1a
Crazytrollgirl <crazytrollgirl@stengg.com>
2022-04-13 16:06:18 +0800

    hotfixed updatetask notes error

--- controllers/updateTaskController.js
+++ controllers/updateTaskController.js
@@ -43,16 +43,26 @@ exports.updatetask = (req, res) => {
 
     // UPDATE APP
     if (confirm_btn){
-        // UPDATE APP
-        db.query('UPDATE task SET description = ?, notes = ?, state = ? WHERE task_id = ?', [description, new_note, state, task_id], (error, result) => {
-            if (error) {
-                console.log(error);
-            }
-            res.render('updatetask', {
-                message: 'Task successfully updated!',
-                isLoggedIn: req.session.isLoggedIn
+        db.query('SELECT * FROM task WHERE task_id = ?', [task_id], (error, result) => {
+             // date that task was created
+             var today = new Date();
+             var createDate = today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate();
+
+            var audit_log = `${result[0].notes}\n ${new_note}, ${req.session.username}, ${createDate}` 
+
+            console.log('audit log >>>>>', audit_log);
+
+            // UPDATE APP
+            db.query('UPDATE task SET description = ?, notes = ?, state = ? WHERE task_id = ?', [description, audit_log, state, task_id], (error, result) => {
+                if (error) {
+                    console.log(error);
+                }
+                res.render('updatetask', {
+                    message: 'Task successfully updated!',
+                    isLoggedIn: req.session.isLoggedIn
+                });
             });
-        });
-        return;
+            return;
+            })
     }
 }
